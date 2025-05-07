a90d3a704cb812fbeb985b41d8fa644a94f3d943
Yingliang <yingliang.24@ichat.sp.edu.sg>
2025-05-07 16:02:40 +0800

    exercise 2 commit

--- Lab3.py
+++ Lab3.py
@@ -8,39 +8,45 @@ def bubble_sort(arr, sorting_order):
 
     # Copy input list to results list
     arr_result = arr.copy()
+    if all(isinstance(i, int) for i in arr):
 
     # Get number of elements in the list
-    n = len(arr_result)
+        n = len(arr_result)
 
-    if n < 10:
+        if n < 10:
         # Traverse through all array elements
-        for i in range(n - 1):
+            for i in range(n - 1):
             # range(n) also work but outer loop will
             # repeat one time more than needed.
 
             # Last i elements are already in place
-            for j in range(0, n - i - 1):
+                for j in range(0, n - i - 1):
 
-                if sorting_order == SORT_ASCENDING:
-                    if arr_result[j] > arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    if sorting_order == SORT_ASCENDING:
+                        if arr_result[j] > arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
 
-                elif sorting_order == SORT_DESCENDING:
-                    if arr_result[j] < arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
-
-                else:
+                    elif sorting_order == SORT_DESCENDING:
+                        if arr_result[j] < arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    else:
                     # Return an empty array
-                    arr_result = []
+                        arr_result = []
+                        return 0 
+               
+        elif n >= 10: 
+            arr_result = 1
+
+        return arr_result
     else:
-        arr_result = -1
-
-    return arr_result
-
+        arr_result = 2
+        
+    return arr_result 
+    
 def main():
     # Driver code to test above
-    arr = [64, 34, 25, 12, 22, 11, 90]
+    arr = [1,2,3,4,5]
 
     # Sort in ascending order
     result = bubble_sort(arr, SORT_ASCENDING)
--- test_Lab3.py
+++ test_Lab3.py
@@ -5,8 +5,8 @@ print("Test_Lab3")
 
 def test_bubble_sort_ascending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [11, 12, 22, 25, 34, 64, 90]
+    input_arr = [1,2,6,5]
+    test_arr = [1,2,5,6]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
 
@@ -14,17 +14,33 @@ def test_bubble_sort_ascending():
 
 def test_bubble_sort_descending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [90, 64, 34, 25, 22, 12, 11]
+    input_arr = [1,2,6,5]
+    test_arr = [6,5,2,1]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
     assert (result == test_arr)
 
-def test_bubble_sort_invalid():
+def test_bubble_sort_too_many_n():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    input_arr = [64, 34, 25, 12, 22, 11, 90,0,0,0,0,0,0,0]
 
-    result = Lab3.bubble_sort(input_arr, 3)
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING )
+
+    assert (result == 1)
+
+def test_bubble_sort_0():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_DESCENDING )
 
     assert (result == [])
+
+def test_bubble_sort_not_integer():
+    result = []
+    input_arr = [1.1]
+
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING )
+
+    assert (result == 2)
